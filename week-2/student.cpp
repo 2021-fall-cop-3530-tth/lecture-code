6a50d6cc486688498b5cf39f32a39fa1348a221e
Steven Bitner <sbitner@uwf.edu>
2021-09-02 09:44:43 -0500

    Adding return to <<

--- week-2/student.cpp
+++ week-2/student.cpp
@@ -10,4 +10,6 @@ Student::Student (double gpa, std::string fname, std::string lname)
 std::ostream& operator<< (std::ostream& foo, const Student& s)
 {
 	foo << s.lname << ", " << s.fname << ": " << s.gpa;
+
+	return foo;
 }
