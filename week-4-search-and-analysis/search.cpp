f907f00f5ea5a6042bd2bedb22ac3872d708a977
Steven Bitner <sbitner@uwf.edu>
2021-09-16 10:42:43 -0500

    Passing two tests

--- week-4-search-and-analysis/search.cpp
+++ week-4-search-and-analysis/search.cpp
@@ -2,5 +2,27 @@
 
 int Search::Find(int arr[], int length, int searchKey)
 {
+	int right = length;
+	int left = 0;
+	int middle = length/2;
+
+	while (right > left)
+	{
+		middle = (left + right) / 2;
+		
+		if (searchKey == arr[middle])
+		{
+			return middle;
+		}
+		else if (searchKey < arr[middle])
+		{
+			right = middle - 1;
+		}
+		else
+		{
+			left = middle + 1;
+		}
+	}
+
 	return -1;
 }
