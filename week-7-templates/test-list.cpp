45d03c3d8c12e1a672cc51eea693149d377028b3
Steven Bitner <sbitner@uwf.edu>
2021-10-07 10:46:47 -0500

    Chain assertions on list insertion

--- week-7-templates/test-list.cpp
+++ week-7-templates/test-list.cpp
@@ -5,6 +5,12 @@
 TEST_CASE("Test List")
 {
 	List<int> l;
+	REQUIRE(nullptr == l.GetFront());
+
 	l.InsertAtFront(10);
 	REQUIRE(10 == l.GetFront()->GetValue());
+
+	l.InsertAtFront(5);
+	REQUIRE(5 == l.GetFront()->GetValue());
+	REQUIRE(10 == l.GetFront()->GetNext()->GetValue());
 }
